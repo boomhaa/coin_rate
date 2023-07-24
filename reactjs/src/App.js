4cc9c83e6c1bf2d071926bad1163765b1c47a859
boomhaa <vtarakanov2017@litsey2.ru>
2023-07-24 17:35:20 +0300

    added tables

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 
+
 function App() {
 
   const [data,setData]=useState([{}])
@@ -14,7 +15,24 @@ function App() {
     <div>
         {(typeof data.courses==='undefined')?(<p>Loading ... </p>):(
           data.courses.map((course,i)=>(
-            <p key={i}>price={course.price} symbol={course.symbol} </p>
+            
+            <table border={1} className="sa">
+              <thead>
+                <tr>
+                  <th>symbol</th>
+                  <th>price</th>
+                </tr>
+              </thead>
+              <tbody key={i}>
+                  <td width={250} align="center">{course.symbol}</td>
+                  <td width={250} align="center"> {course.price} </td>
+
+              </tbody>
+            
+            </table>
+
+            
+            
           ))
         )}
     </div>
